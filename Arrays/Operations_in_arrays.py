56e3ef47d6f4a7aa209b20d68f53dd8fd6ccf620
Kd-Here <gamelover9899@gmail.com>
2022-09-14 13:32:41 +0530

    Operation in Arrays

---
+++ Arrays/Operations_in_arrays.py
@@ -0,0 +1,9 @@
+
+
+
+# 1) Arrays creation   :- a = [] created a array
+# 2) Array append      :- a.append(9899)  it means 9899 is appended (added at end index)
+# 3) Array insert      :- a.insert(insert_at_index,element)        a.insert(2,9899)
+# 4) Array pop         :- a.pop(index)  removes element at given index
+# 5) Array remove      :- a.remove(element) removes first occurence of element 
+# 6) Reverse array     :- a.reverse() print(a) this function reverse the array 
