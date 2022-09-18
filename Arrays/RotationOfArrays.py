a99c97b92061248c2c8d6bc8fb6df7f6c52d089f
Kd-Here <90677747+Kd-Here@users.noreply.github.com>
2022-09-18 19:16:49 +0530

    Created rotation function

--- Arrays/RotationOfArrays.py
+++ Arrays/RotationOfArrays.py
@@ -14,3 +14,18 @@ for i in range(int(input())):
     l2.append(arry[0])
     print(l2)
     arry = l2
+    
+    
+    
+#   Fastest way is to remove term till rotate number and put them at last
+# Here first and second number are removed boz of rotate = 2 and put in back 
+# Just remember we used list addition
+l1 = [1,2,3,4,5]
+
+def ArrayRotate(a,b):
+    l2 = []
+    l2 = l1[(rotate):]+l1[0:(rotate)]
+    return l2
+rotate = int(input())
+ArrayRotate(l1,rotate)
+
