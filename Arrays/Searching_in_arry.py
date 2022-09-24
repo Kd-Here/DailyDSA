f48393393caa04873d75976edbdb1cf89d4c4841
Kd-Here <gamelover9899@gmail.com>
2022-09-25 00:49:12 +0530

    Please find source code for builtin funct

--- Arrays/Searching_in_arry.py
+++ Arrays/Searching_in_arry.py
@@ -1,3 +1,9 @@
+# Tommorws task is to find source code of python various built in function & methods.
+
+
+
+
+
 #------------------------- My approacch ------------------------------
 arr = list(map(int,input().split()))
 findx = int(input())
@@ -8,11 +14,61 @@ if findx in arr:
 else:
     print(f'Not found')
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # --------------Creating my index function we have index as method we are creating a function then we will create a methods-------------------------
-def myindex(arry,elementIndexToFind):
-    for i in range(len(arry)):
-        if elementIndexToFind == arry[i]:
-            return i
-        else:
-            return "Not present"
-print(muy)
+# def myindex(arry,elementIndexToFind):
+#     i = 0; b =  len(arry) 
+#     while(i < b):
+#         if elementIndexToFind == arry[i]:
+#             return i+1
+#         i += 1
+#     else:
+#         return "Not present"
+
+# print(myindex(a,9))
+# a = [1,2,3,4,5,6,7]
+
+
+
+
+# ___________________________________Created MyIndex class who has method of Findindex_____________________________
+# class MyIndex:
+#     def __init__(self,arry,elementIndexToFind):
+#         self.Kd_aryy = arry
+#         self.ElementIndexToFind = elementIndexToFind
+    
+#     def Findindex(self,ass):
+#         for i in range(len(self.Kd_aryy)):
+#             if self.Kd_aryy[i] == ass:
+#                 print(i+1)
+#                 break
+#         else:
+#             print("No Found")
+
+
+# givenArry = list(map(int,input("Give me arry:- ").split()))
+# Getindexof = int(input("Give element whose index you want:- "))
+
+# a = MyIndex(givenArry,8)
+# a.Findindex(Getindexof)
