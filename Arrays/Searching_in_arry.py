32a14e3c2661f9e3138d305e3342a4337c5b1066
Kd-Here <gamelover9899@gmail.com>
2022-09-25 00:00:38 +0530

    Myindex

--- Arrays/Searching_in_arry.py
+++ Arrays/Searching_in_arry.py
@@ -1,7 +1,4 @@
 #------------------------- My approacch ------------------------------
-from operator import index
-
-
 arr = list(map(int,input().split()))
 findx = int(input())
 print(arr)
@@ -10,3 +7,12 @@ if findx in arr:
     print(arr.index(findx))
 else:
     print(f'Not found')
+
+# --------------Creating my index function we have index as method we are creating a function then we will create a methods-------------------------
+def myindex(arry,elementIndexToFind):
+    for i in range(len(arry)):
+        if elementIndexToFind == arry[i]:
+            return i
+        else:
+            return "Not present"
+print(muy)
