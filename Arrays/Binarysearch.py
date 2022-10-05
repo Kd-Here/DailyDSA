6fed48168fc1b6b6831134283217d2465f931a47
Kd-Here <gamelover9899@gmail.com>
2022-10-05 21:57:08 +0530

    Sorry Kd not solve any problem here please do tommorow

--- Arrays/Binarysearch.py
+++ Arrays/Binarysearch.py
@@ -1,17 +1,11 @@
-
 array = [1,2,3, 4, 5, 6, 7, 8, 9]
 x = int(input())
 h = len(array)-1
-
-
-
-
 
 def binarySearch(array, x, low, high):
 
     # Repeat until the pointers low and high meet each other
     while (low <= high):
-
         mid = (low + high)//2
 
         if array[mid] == x:
@@ -28,3 +22,9 @@ def binarySearch(array, x, low, high):
 
 result = binarySearch(array, x, 0, h)
 print(result)
+
+
+# #########################
+# Binary Search with Recursive method:-
+# #########################
+
