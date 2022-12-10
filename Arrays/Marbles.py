8ebd525ae7f95fb1b4f5f6484825590490e3ed9e
Kd-Here <gamelover9899@gmail.com>
2022-12-10 20:58:02 +0530

    Gitstatus

--- Arrays/Marbles.py
+++ Arrays/Marbles.py
@@ -7,27 +7,31 @@ arr = [1,-2,-3,4,-8,8,-9,-10,9,-10]
 # #     else:
 # #         Compare(x,(y-1))
 
-# # Compare(a,-1)
+arr = [-9,-1,-4,-2,30,4,5,8]
 
-# # Play with position of even and odd
+
+arr = [-9,-1,-4,-2,30,4,5,8]
 
 for i in range(len(arr)):
     if i%2 == 0: #Even
         if arr[i]<0:    #Even with -ve
-            for j in arr[i:]:
-                if j >0:
-                    c = arr.pop(i)
-                    arr.insert(i+1,c)
+            for j in range(len(arr[i:])):
+                if arr[j] >0:
+                    arr[j] = arr[j] + arr[i]
+                    arr[i] = arr[j] - arr[i]
+                    arr[j] = arr[j] - arr[i]
                     #remove element at the index
                     break
 
     else: #Odd
         if arr[i] > 0:  #Odd with +ve
-            for j in arr[i:]:
-                if j <0:
-                    c = arr.pop(i)
-                    arr.insert(i+1,c)
+            for j in range(len(arr[i:])):
+                if arr[j] <0:
+                    arr[j] = arr[j] + arr[i]
+                    arr[i] = arr[j] - arr[i]
+                    arr[j] = arr[j] - arr[i]
                     break
 print(arr)
-
 # # 
+
+
